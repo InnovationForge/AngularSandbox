908da9d20064312b585d6c9b4a52436c765d7a2d
Niranjan Rath <niranjan.rath@rabobank.nl>
2023-07-31 14:18:12 +0200

    modules organized

--- multi-module-todo-list-app/src/app/core/header/header.component.ts
+++ multi-module-todo-list-app/src/app/core/header/header.component.ts
@@ -11,11 +11,4 @@ export class HeaderComponent {
   // Add any logic or properties specific to the header component here.
 
   constructor(private router: Router) {}
-  public onTestClick(){
-    console.log("link clicked")
-  }
-
-  showTaskCreate() {
-    this.router.navigate(['/tasks/new']); // Navigate to the task details page with the task ID as a parameter
-  }
 }
