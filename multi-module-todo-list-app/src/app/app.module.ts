4c925b014475685f5fdac83123e05921c3a552fd
Niranjan Rath <niranjan.rath@rabobank.nl>
2023-07-31 14:17:33 +0200

    modules organized

--- multi-module-todo-list-app/src/app/app.module.ts
+++ multi-module-todo-list-app/src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
 import {TaskModule} from "./task/task.module";
 import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     TaskModule,
     SharedModule,
     AppRoutingModule,
--- multi-module-todo-list-app/src/app/core/header/header.component.ts
+++ multi-module-todo-list-app/src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {TasksService} from "../../services/tasks.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   // Add any logic or properties specific to the header component here.
+
+  constructor(private router: Router) {}
+  public onTestClick(){
+    console.log("link clicked")
+  }
+
+  showTaskCreate() {
+    this.router.navigate(['/tasks/new']); // Navigate to the task details page with the task ID as a parameter
+  }
 }
